bbdd3dccb2cc2b12950894b5e607813af08a52c6
Jade Piramide <jadepiramidepogi@gmail.com>
2020-03-20 18:30:44 -0700

    Started the base of the game with a basic Song metadata holder and the beginning of a Conductor which controls the rhythm of the game in order to keep the game in sync with the music.

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+
+  private Song song;
+  private float crotchet;
+  private float dspAtStart;
+  private float songPosition;
+  private bool songActive = false;
+
+  public void RegisterSong(Song songToRegister)
+  {
+    song = songToRegister;
+    crotchet = 60f / song.bpm; // frames per bpm
+  }
+
+  public void BeginSong()
+  {
+    dspAtStart = AudioSettings.dspTime;
+    songActive = true;
+  }
+
+  private void Update()
+  {
+    if (!songActive)
+      return;
+
+    songPosition = (AudioSettings.dspTime - dspAtStart) * song.pitch - song.offset;
+  }
+
+}
---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// The <c>Song</c> ScriptableObject.
+/// This class contains all metadata concerning songs and charts including
+/// BPM, crotchet, song length, etc.
+/// </summary>
+[CreateAssetMenu(menuName = "Song", fileName = "New Song")]
+public class Song : ScriptableObject
+{
+
+  [Header("Song Data")]
+  public string songName = "New Song";
+  public float bpm;
+  public float offset;
+  public Sprite songVisual;
+
+  [Header("Song Audio")]
+  public AudioClip audio;
+  [Range(0f, 5f)]
+  [Tooltip("How much the volume of the song is scaled during gameplay. This is useful for altering the volume of loud or quiet songs to make them nicer to listen to.")]
+  public float volumeModifier = 1f;
+  public float pitch = 1f;
+
+}
